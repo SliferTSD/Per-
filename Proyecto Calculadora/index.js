4cbb974e6be39744ae8ca28ed09281c767277845
SliferTSD <slifertheskydragon750@gmail.com>
2024-11-28 23:38:08 -0300

    "Actualización en interactividad del JS - Por ahora hice que mostrara un mensaje en cada "botón" del div que es clickeado"

--- Proyecto Calculadora/index.js
+++ Proyecto Calculadora/index.js
@@ -1,5 +1,7 @@
 window.alert("Bienvenido papu a la calculadora fantasticamente megagaláctica");
-const myDiv = document.querySelector('#1');
-myDiv.addEventListener('click', function() {
-    window.alert('Div was clicked!');
-  });
+const btn=document.querySelectorAll(".digitos");
+btn.forEach(btn => {
+    btn.addEventListener('click', function alerta(){
+    window.alert("Salio un mensaje salvaje!!!");
+    });
+});
