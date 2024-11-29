066a6420af0bcc0991d712dc704f0b669db1d566
SliferTSD <slifertheskydragon750@gmail.com>
2024-11-28 22:54:01 -0300

    "Prueba de click en div con JS"

--- Proyecto Calculadora/index.js
+++ Proyecto Calculadora/index.js
@@ -1,1 +1,5 @@
 window.alert("Bienvenido papu a la calculadora fantasticamente megagaláctica");
+const myDiv = document.querySelector('#1');
+myDiv.addEventListener('click', function() {
+    window.alert('Div was clicked!');
+  });
